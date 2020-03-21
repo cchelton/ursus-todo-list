de2eb44a26ec243238359fe2c68e6ec8ed89ea62
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-21 12:36:37 -0500

    complete button changes class

--- client.js
+++ client.js
@@ -19,20 +19,30 @@ function addTask(event) {
 }
 
 function completeTask() {
-  console.log($(this).parent()); // TODO: Finish getting data from index
+  let i = $(this).data("index");
+  tasks[i].completed = !tasks[i].completed;
+  renderList();
 }
 
 function renderList() {
   $(".js-task-ul").empty();
   for (let i = 0; i < tasks.length; i++) {
-    $(".js-task-ul")
-      .append(
+    if (tasks[i].completed) {
+      $(".js-task-ul").append(
         `
-    <li>
-        ${tasks[i].name}<button class="js-taskComplete-btn">Complete</button>
-      </li>
-    `
-      )
-      .data("index", i);
+      <li class="isComplete">
+          ${tasks[i].name}<button  data-index=${i} class="js-taskComplete-btn btn">Complete</button>
+        </li>
+      `
+      );
+    } else {
+      $(".js-task-ul").append(
+        `
+      <li>
+          ${tasks[i].name}<button  data-index=${i} class="js-taskComplete-btn btn">Complete</button>
+        </li>
+      `
+      );
+    }
   }
 }
