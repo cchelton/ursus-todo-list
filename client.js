394a6cd6269e3323ccda90480b23a51c30561717
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-21 12:46:49 -0500

    comments and removed logs

--- client.js
+++ client.js
@@ -3,12 +3,12 @@ const tasks = [];
 $(document).ready(init);
 
 function init() {
-  console.log("jq-on");
   $(".js-addTask-form").on("submit", addTask);
   $(".js-task-ul").on("click", ".js-taskComplete-btn", completeTask);
 }
 
 function addTask(event) {
+  //  adds task to tasks array on form submit
   event.preventDefault();
   tasks.push({
     name: $(".js-addTask-input").val(),
@@ -19,15 +19,18 @@ function addTask(event) {
 }
 
 function completeTask() {
+  //  inverts task object completed property
   let i = $(this).data("index");
   tasks[i].completed = !tasks[i].completed;
   renderList();
 }
 
 function renderList() {
+  //  renders js-task-ul to screen
   $(".js-task-ul").empty();
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].completed) {
+      //  complete task, disabled button
       $(".js-task-ul").append(
         `
       <li class="isComplete">
@@ -36,6 +39,7 @@ function renderList() {
       `
       );
     } else {
+      //  incomplete task, complete button is enabled
       $(".js-task-ul").append(
         `
       <li>
